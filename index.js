56e075ccdd196ae68d08431353d73feec7a28717
Hakam Royhan Adiluhung <72461302+hradiluhung@users.noreply.github.com>
2021-11-10 22:18:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const nama = document.querySelector("#name-input .input-field");
 const email = document.querySelector("#email-input .input-field");
 const password = document.querySelector("#password-input .input-field");
 const confirmPassword = document.querySelector("#password-confirm-input .input-field");
+const agreementCheck = document.getElementById("agreement-check");
 const submitBtn = document.getElementById("submit-btn");
 
 function changeLine(fromInput, toInput){
@@ -27,3 +28,31 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirmPassword.onkeyup = validatePassword;
+
+let inputData = [[nama, "Name"], [email, "Email"], [password, "Password"], [confirmPassword, "Password Confirm"], [agreementCheck, "Privacy Policy Agreement"]];
+let kosong = [];
+let printAlert = "";
+
+submitBtn.addEventListener("click", function(event){
+  if(nama.value == "" || email.value == "" || password.value == "" || confirmPassword.value == "" || agreementCheck.checked == false){
+    event.preventDefault();
+    printAlert = "";
+    kosong = [];
+    for(let i = 0 ; i < inputData.length; i++){
+      if(inputData[i][0].value=="" || inputData[i][0].checked == false){
+        kosong.push(inputData[i][1]);
+      }
+    }
+    for(let i = 0; i < kosong.length; i++){
+      if(i == kosong.length - 1){
+        printAlert += kosong[i] + " ";
+      }else{
+        printAlert += kosong[i] + ", ";
+      }
+    }
+    alert(printAlert + "must be inputted");
+    return false;
+  }else{
+    return true;
+  }
+})
